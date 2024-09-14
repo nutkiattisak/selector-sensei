00c32d1de36712a334d5dccd117b61bae87c9727
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2024-09-14 19:08:02 +0700

    feat: update gtag

--- .vitepress/shared.ts
+++ .vitepress/shared.ts
@@ -2,7 +2,24 @@ import { defineConfig, type HeadConfig } from "vitepress"
 
 export const shared = defineConfig({
   title: "Selector Sensei",
-  head: [["link", { rel: "icon", href: "/assets/selector-sensei-logo.png" }]],
+  head: [
+    ["link", { rel: "icon", href: "/assets/selector-sensei-logo.png" }],
+    [
+      "script",
+      {
+        async: "",
+        src: "https://www.googletagmanager.com/gtag/js?id=GTM-MK8WHMCS",
+      },
+    ],
+    [
+      "script",
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'GTM-MK8WHMCS');`,
+    ],
+  ],
   rewrites: {
     "th/:rest*": ":rest*",
   },
